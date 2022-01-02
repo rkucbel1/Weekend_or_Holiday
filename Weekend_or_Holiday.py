14fceabe71ddf589cdeb6eee5c66d6500406089f
Roman Kucbel <65679878+rkucbel1@users.noreply.github.com>
2022-01-01 20:59:39 -0700

    Update Weekend_or_Holiday.py
    updated holiday schedule from 2021 to 2022

--- Weekend_or_Holiday.py
+++ Weekend_or_Holiday.py
@@ -6,13 +6,18 @@ import os
 token = os.environ.get('PA_API_TOKEN')
 url_api = os.environ.get('LINK_WEEKEND_OR_HOLIDAY')
 
-#CME 2021 Holiday Schedule
-H1 = '2021-01-18'
-H2 = '2021-02-15'
-H3 = '2021-05-31'
-H4 = '2021-07-05'
-H5 = '2021-11-21'
-H6 = '2021-12-24'
+#CME 2022 Holiday Schedule
+H1 = '2022-01-03'
+H2 = '2022-01-17'
+H3 = '2022-02-21'
+H4 = '2022-04-15'
+H5 = '2022-05-30'
+H6 = '2022-06-20'
+H7 = '2022-07-04'
+H8 = '2022-09-05'
+H9 = '2022-11-24'
+H10 = '2022-12-26'
+H11 = '2023-01-02'
 
 #Get if weekend or not
 day_of_week = datetime.datetime.today().weekday()
@@ -26,11 +31,11 @@ else:
 today = datetime.date.today()
 today = str(today)
 
-if (today == H1) or (today == H2) or (today == H3) or (today == H4) or (today == H5) or (today == H6):
+if (today == H1) or (today == H2) or (today == H3) or (today == H4) or (today == H5) or (today == H6) or (today == H7) or (today == H8) or (today == H9) or (today == H10) or (today == H11) :
    holiday = True
 else:
    holiday = False
-
+   
 #Update date and status in database
 #There is only one element in this database
 id_number = 1
